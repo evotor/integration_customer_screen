246d623810872f6897f1be14a986488c9bb522fd
a.bogdanov <a.bogdanov@evotor.ru>
2021-09-14 17:13:59 +0300

    startActivity stubs

--- app/src/main/java/ru/evotor/external/integrations/library/CustomerScreenService.java
+++ app/src/main/java/ru/evotor/external/integrations/library/CustomerScreenService.java
@@ -2,6 +2,7 @@ package ru.evotor.external.integrations.library;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.RemoteException;
@@ -36,6 +37,21 @@ public abstract class CustomerScreenService extends Service {
         } finally {
             p.recycle();
         }
+    }
+
+    @Override
+    public void startActivities(Intent[] intents) {
+        throw new RuntimeException("Please, use startActivity(Intent intent) for start your activity!");
+    }
+
+    @Override
+    public void startActivities(Intent[] intents, Bundle options) {
+        throw new RuntimeException("Please, use startActivity(Intent intent) for start your activity!");
+    }
+
+    @Override
+    public void startActivity(Intent intent, Bundle options) {
+        throw new RuntimeException("Please, use startActivity(Intent intent) for start your activity!");
     }
 
     protected abstract String execute(String request) throws Throwable;
